a5929d68b4631ff69f230dc97287d7bb0c49dbce
Julie <julielee1115@gmail.com>
2017-08-02 10:21:48 -0400

    eeeee

--- meetups.py
+++ meetups.py
@@ -5,13 +5,21 @@ API_Key = AIzaSyCnuCETkfIGLknIdLADpe5v2l62mogdHLE
 
 google_places = GooglePlaces(API_Key)
 
+#get address from alexa, geocode to coordinates
+result = Geocoder.geocode("7250 South Tucson Boulevard, Tucson, AZ 85756")
+if(result.valid_address == False):
+    return "please enter a valid address"
 
 
-#
+
+#nearby search, get keyword?
 query_result = google_places.nearby_search(
-    location='Mumbai', keyword='Restaurants',
+    location= result, keyword='Restaurants',
     radius=1000, types=[types.TYPE_RESTAURANT])
 
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+if query_result.has_attributions:
+   print query_result.html_attributions
 
-result = Geocoder.geocode("7250 South Tucson Boulevard, Tucson, AZ 85756")
+
+for place in query_result.places:
+    print place.name
