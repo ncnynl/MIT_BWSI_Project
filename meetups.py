d9488787b62dd67ba2ee7dbabbdcb711295750cb
Julie <julielee1115@gmail.com>
2017-08-04 16:08:09 -0400

    SDLKFJLS;KFJA;LSKDJFA;LKSDJFL;KDJFADL;JA DONE

--- meetups.py
+++ meetups.py
@@ -10,17 +10,15 @@ import json
 import pprint
 
 
-API_Key = "AIzaSyDjIdmWA4l-U9zAalsg9ySB3vy1qBxdJZs"
-gmaps = googlemaps.Client(key=API_Key)
-lato = 0
-lono = 0
-latd = 0
-lond = 0
-first = set()
-sec = set()
+
 
 app = Flask(__name__)
 ask = Ask(app, '/')
+
+API_Key = "AIzaSyDjIdmWA4l-U9zAalsg9ySB3vy1qBxdJZs"
+gmaps = googlemaps.Client(key=API_Key)
+first = set()
+sec = set()
 
 @app.route('/')
 def homepage():
@@ -32,30 +30,35 @@ def start_skill():
     return question(msg)
 
 @ask.intent("FirstAddressSavingIntent")
-def saveFAddress(Address):
-    session.attributes["FAddress"] = Address
+def saveFAddress(FAddress):
+    session.attributes["FAddress"] = FAddress
     addr = session.attributes["FAddress"]
+    print(addr, flush = True)
     geolocator = Nominatim()
-    location = geolocator.geocode('addr')
-    lato = location.latitude
-    lono = location.longitude
+    location = geolocator.geocode(addr)
+    session.attributes["lato"] = location.latitude
+    session.attributes["lono"] = location.longitude
+    print(session.attributes["lato"], session.attributes["lono"])
 
     msg = "What is the second address?"
     return question(msg)
 
 
 @ask.intent("SecondAddressSavingIntent")
-def saveSAddress(Address):
-    session.attributes["SAddress"] = Address
+def saveSAddress(SAddress):
+    session.attributes["SAddress"] = SAddress
     addr = session.attributes["SAddress"]
+    print(addr, flush = True)
     geolocator = Nominatim()
-    location = geolocator.geocode('addr')
-    latd = location.latitude
-    lond = location.longitude
-    twothirdr = radiuscalc(lato, lono, latd, lond)
-    fset = nearbysearch(lato, lono, twothirdr)
-    sset = nearbysearch(latd, lond, twothirdr)
+    location = geolocator.geocode(addr)
+    session.attributes["latd"] = location.latitude
+    session.attributes["lond"] = location.longitude
+    print(session.attributes["lato"], session.attributes["lono"], session.attributes["latd"], session.attributes["lond"], flush = True)
+    twothirdr = radiuscalc(session.attributes["lato"], session.attributes["lono"], session.attributes["latd"], session.attributes["lond"])
+    fset = nearbysearch(session.attributes["lato"], session.attributes["lono"], twothirdr)
+    sset = nearbysearch(session.attributes["latd"], session.attributes["lond"], twothirdr)
     finaldest = fset.intersection(sset)
+    print(finaldest)
     dest_msg = "My recommended locations are {}".format(finaldest)
     return statement(dest_msg)
 
@@ -66,8 +69,10 @@ def radiuscalc(lao, loo, lad, lod):
 
     response = urlopen(url).read().decode('utf8')
     obj = json.loads(response)
-    radius = (obj['rows'][0]['elements'][0]['distance']['text'])
-    fradius = radius * 2/3
+    print(obj)
+    radius = float((obj['rows'][0]['elements'][0]['distance']['text']).split(" ")[0])
+    print(radius)
+    fradius = radius * 2/3 * 1000
     return fradius
 
 def nearbysearch(lat, lon, rad):
@@ -79,4 +84,8 @@ def nearbysearch(lat, lon, rad):
     places = set()
     for i in range(total):
         places.add(result[i]['name'])
+    print(places)
     return places
+
+if __name__ == '__main__':
+    app.run(debug=True)
